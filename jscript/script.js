6b88e0e174c0557fc836cbf0e6546819b523416a
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-01 15:38:54 +0100

    Inizio progetto

---
+++ jscript/script.js
@@ -0,0 +1,46 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// prezzo biglietto 0.21€ al km
+// scontro 20% under 18 - sconto 40% over 65
+// risultato con 2 decimali
+
+//setup
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("okay");
